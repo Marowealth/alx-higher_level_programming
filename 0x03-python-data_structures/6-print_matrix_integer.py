bab64913bc33fe4f5df12ff2a92c787add7cfb5c
Marowealth <marolovey9@gmail.com>
2022-08-29 19:42:21 -0700

    all

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 6-print_matrix_integer.py
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row:
+            if i is not row[len(row) - 1]:
+                print("{:d}".format(i), end=" ")
+            else:
+                print("{:d}".format(i), end="")
+        print()
